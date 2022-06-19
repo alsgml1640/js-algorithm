d3d0c70d86e38e0d87268b58f73b6e5a879e1f3b
정민희 <dev.alsgml1640@gmail.comm>
2022-06-20 02:33:55 +0900

    Update k번째.js

--- 프로그래머스/k번째.js
+++ 프로그래머스/k번째.js
@@ -1,5 +1,10 @@
 function solution(array, commands) {
     let result = [];
+    // commands.map((e,index)=>{
+    //     let i = e[index];
+        
+    //     console.log(i);
+    // })
     for(let l = 0 ; l <commands.length ; l ++ ){
         let i = commands[l][0];
         let j = commands[l][1];
@@ -8,7 +13,7 @@ function solution(array, commands) {
         result.push(array.slice(i-1,j).sort((a,b)=> a- b)[k-1]);
     }
     var answer = result;
-     return answer;
+    return answer;
 }
 
 console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
