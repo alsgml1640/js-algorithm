1967c53115945a51b59692d718727fcd7bd95a4f
정민희 <dev.alsgml1640@gmail.comm>
2022-06-20 02:53:53 +0900

    Create 2750.js
    백준 정렬 2750 수 정렬하기
    readline방식으로 풀기

---
+++ baekjoon/2750.js
@@ -0,0 +1,30 @@
+//수 정렬하기 
+
+
+// readline 방식으로 풀기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+let N = 0;
+let input = [];
+let result = [];
+rl.on('line',function(line){
+    if(N === 0){
+        N = line;
+    } else {
+        input.push(line);
+        if(input.length === parseInt(N)){
+            result = Array.from(new Set(input)).sort((a,b)=> a- b);
+            result.forEach(e => console.log(e));
+            rl.close();
+        }
+        
+    } 
+});
+
+
+
